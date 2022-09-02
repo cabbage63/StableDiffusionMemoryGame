3122d5cf15801a73d840c3b76217bdc64d2e364c
cabbage63 <7237868+cabbage63@users.noreply.github.com>
2022-09-02 20:48:09 +0900

    Show Image on the card

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 const board = [];
+const cardNumX = 5;
+const cardNumY = 5;
+const cardSize = 200;
+const cardMargin = 10;
 const init = () => {
     const container = document.createElement("div");
     container.style.position = "relative";
     document.body.appendChild(container);
 
-    for(let y = 0; y < 3; y++){
+    for(let y = 0; y < cardNumY; y++){
         board[y] = [];
-        for(let x = 0; x < 3; x++){
+        for(let x = 0; x < cardNumX; x++){
             const panel = document.createElement("div");
             panel.style.position = `absolute`;
-            panel.style.left = `${x * 100 + 2}px`;
-            panel.style.top = `${y * 100 + 2}px`;
-            panel.style.width = `96px`;
-            panel.style.height = `96px`;
+            panel.style.left = `${x * cardSize}px`;
+            panel.style.top = `${y * cardSize}px`;
+            panel.style.width = `${cardSize-cardMargin}px`;
+            panel.style.height = `${cardSize-cardMargin}px`;
             panel.style.backgroundColor = `#f00`;
+            panel.style.backgroundSize = `cover`
+            panel.imageURL = `url("./assets/00000.png")`
             panel.style.borderRadius = `10px`;
             panel.style.transition = `all 150ms linear`;
             container.appendChild(panel);
@@ -28,7 +34,7 @@ const init = () => {
 
 let isAnimation = false
 const flip = async(x, y) => {
-    if(x < 0 || y < 0 || x >= 3 || y >= 3){
+    if(x < 0 || y < 0 || x >= cardNumX || y >= cardNumY){
         return;
     }
     isAnimation = true;
@@ -40,6 +46,7 @@ const flip = async(x, y) => {
     panel.style.transform = "perspective(150px) rotateY(0deg)"
     await new Promise(r => setTimeout(r, 150));
     panel.style.backgroundColor = (color) ? "#00f" : "#f00"
+    panel.style.backgroundImage = (color) ? panel.imageURL : null
     panel.style.transform = "perspective(150px) rotateY(-90deg)"
     panel.parentElement.appendChild(panel);
     await new Promise(r => setTimeout(r, 50));
@@ -60,10 +67,6 @@ const ondown = (x, y) => {
         return;
     }
     flip(x, y);
-    flip(x + 1, y);
-    flip(x - 1, y);
-    flip(x, y + 1);
-    flip(x, y - 1);
 
     isGameOver = board.flat().every((v) => v.color === 1);
 };
